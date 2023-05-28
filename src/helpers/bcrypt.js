b70be34add6930a9f03f4c79452a372560918343
4shx1 <ashwin.biju.k@gmail.com>
2023-05-28 22:02:09 +0530

    updated bcrypt file

--- src/helpers/bcrypt.js
+++ src/helpers/bcrypt.js
@@ -1,12 +1,24 @@
 const bcrypt = require('bcrypt');
 
+// Function to hash the password
 const hashPassword = async (password) => {
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         return hashedPassword;
     } catch (error) {
         throw new Error('Error hashing password');
     }
 };
 
-module.exports = hashPassword;
+// Function to compare the password
+const comparePassword = async (password, hashedPassword) => {
+    try {
+        const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+        return isPasswordValid;
+    } catch (error) {
+        throw new Error('Error comparing passwords');
+    }
+};
+
+module.exports = { hashPassword, comparePassword };
