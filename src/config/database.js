a33540205180604a14964dbad69cfb2bbbfd1643
4shx1 <ashwin.biju.k@gmail.com>
2023-05-28 22:22:05 +0530

    updated database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
     try {
-        await mongoose.connect('mogodbUrl', {
+        await mongoose.connect('mongodb+srv://hallmanager20:5FXYmGga7DfwBq9J@cluster0.uk8l5b3.mongodb.net/myDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
